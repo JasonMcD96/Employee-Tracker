e263dc6caeb4e92a1824b73acdbc70294ba551f4
Jason McDonald <jasonmcd65@gmail.com>
2021-01-15 20:46:39 -0600

    sql code for initial DB setup

---
+++ assets/Employee_Tracker_db.sql
@@ -0,0 +1,8 @@
+-- Drops the database if it already exists --
+DROP DATABASE IF EXISTS employee_tracker_db;
+
+-- create the database --
+CREATE DATABASE employee_tracker_db;
+
+-- use database employee_tracker_db --
+USE employee_tracker_db;
