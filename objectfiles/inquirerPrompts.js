86b64839fbb3fb6293cbfd1f22c61c1cad2022f0
Jason McDonald <jasonmcd65@gmail.com>
2021-01-17 16:37:53 -0600

    contains prompts that do not have dynamic options, cuts down bulk of server.js

---
+++ objectfiles/inquirerPrompts.js
@@ -0,0 +1,36 @@
+class Prompts {
+    constructor(){
+        this.mainPrompt = [
+            {
+                type: 'list',
+                name: 'action',
+                message: 'What would you like to do?',
+                choices: [
+                    'View All Employees',
+                    'View All Departments',
+                    'View All Roles',
+                    'View By Manager',
+                    'Add Employee',
+                    'Add Department',
+                    'Add New Role',
+                    'Update Employee Role',
+                ]
+            },
+            {
+                type: 'input',
+                name: 'newDepartment',
+                message: 'What is the name of the new department?',
+                when: (answers) => answers.action === "Add Department"
+            },
+            {
+                type: 'input',
+                name: 'newRole',
+                message: 'What is the name of the new role?',
+                when: (answers) => answers.action === "Add New Role"
+            }
+        ];
+
+    }
+}
+
+module.exports = Prompts;
