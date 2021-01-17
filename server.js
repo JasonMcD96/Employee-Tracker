1c41b775652d708ca46bd84e7fa852ab2ee65891
Jason McDonald <jasonmcd65@gmail.com>
2021-01-17 09:49:21 -0600

    removed while loop

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const inquirer = require("inquirer")
 const mysql = require("mysql")
+const util = require("util")
 
 let connection = mysql.createConnection({
     host: "localhost",
@@ -13,10 +14,10 @@ connection.connect(function (err) {
     if (err) throw err;
 })
 
+connection.query = util.promisify(connection.query);
 
 
-
-const teamCollection = async (inputs = []) => {
+const responseCollection = async (inputs = []) => {
     const prompts = [
         {
             type: 'list',
@@ -36,53 +37,6 @@ const teamCollection = async (inputs = []) => {
                 'Remove Role'
             ]
         },
-        // {
-        //     type: 'input',
-        //     name: 'firstName',
-        //     message: 'Team member first name:'
-        // },
-        // {
-        //     type: 'input',
-        //     name: 'lastName',
-        //     message: 'Team member last name:'
-        // },
-        // {
-        //     type: 'input',
-        //     name: 'id',
-        //     message: 'What is their employee ID?'
-        // },
-        // {
-        //     type: 'input',
-        //     name: 'email',
-        //     message: 'What is their email'
-        // },
-        // //Intern question(s)
-        // {
-        //     type: 'input',
-        //     name: 'school',
-        //     message: 'What school do they go to?',
-        //     when: (answers) => answers.role === 'Intern'
-        // },
-        // // Manager
-        // {
-        //     type: 'input',
-        //     name: 'officeNumber',
-        //     message: 'What is their office number?',
-        //     when: (answers) => answers.role === 'Manager'
-        // },
-        // //Engineer
-        // {
-        //     type: 'input',
-        //     name: 'github',
-        //     message: 'What is their github?',
-        //     when: (answers) => answers.role === 'Engineer'
-        // },
-        // {
-        //     type: 'confirm',
-        //     name: 'again',
-        //     message: 'Enter another team member?',
-        //     default: 'true'
-        // }
     ];
 
     const answers = await inquirer.prompt(prompts)
@@ -92,82 +46,89 @@ const teamCollection = async (inputs = []) => {
 let getAllEmployees = () => {
     // Get all employees and their information from the database
     console.log("--- Accessing all employees ---")
-    // connection.query(`select employee.first_name,
-    // employee.last_name,
-    // role.title,
-    // department.name,
-    // role.salary
-    // from employee inner join role ON employee.role_id = role.id
-    // inner join department ON role.department_id = department.id;`, function (err, res) {
-    //     console.log(res)
-    // })
+
+    return connection.query(`select a.id, 
+    a.first_name,
+    a.last_name,
+    role.title,
+    department.name,
+    role.salary,
+    concat(b.first_name, ' ', b.last_name)
+    from employee a inner join role ON a.role_id = role.id
+    inner join department ON role.department_id = department.id
+    left join employee b on a.manager_id = b.id;`)
+
 }
 
-let parseResponse = (action) => {
-    switch (action) {
-        case "View All Employees":  // Min
-            getAllEmployees();
-            break;
+let viewEmployees = async () => {
+    let employees = await getAllEmployees()
+    console.log(employees)
 
-        case "View By Department": // Min
-
-            break;
-
-        case "View By Manager": // Min
-
-            break;
-
-        case "View All Roles": // Min
-
-            break;
-
-        case "Update Employee Role": // Min
-
-            break;
-
-        case "View Departments": // Min
-
-            break;
-
-        case "Add Employee":
-
-            break;
-
-        case "Add Employee":
-
-            break;
-
-        case "Remove Employee":
-
-            break;
-
-        case "Update Employee Manager":
-
-            break;
-
-        case "Add New Role":
-
-            break;
-
-        case "Remove Role":
-
-            break;
-
-
-        default:
-
-            break;
-    }
+    // await responseCollection(); // other prompts pertaining what they wanted depending on the context of the situation
+    main()
 }
 
 const main = async () => {
-    while (true) {
-        const input = await teamCollection().then(res => {
-            console.log(res)
-            parseResponse(res.action)
+
+    // viewEmployees();
+
+    //  while (true) {
+        let answers = await responseCollection().then(answers => {
+
+            switch (answers.action) {
+                case "View All Employees":  // Min
+                    viewEmployees();
+                    break;
+
+                case "View By Department": // Min
+
+                    break;
+
+                case "View By Manager": // Min
+
+                    break;
+
+                case "View All Roles": // Min
+
+                    break;
+
+                case "Update Employee Role": // Min
+
+                    break;
+
+                case "View Departments": // Min
+
+                    break;
+
+                case "Add Employee":
+
+                    break;
+
+                case "Add Employee":
+
+                    break;
+
+                case "Remove Employee":
+
+                    break;
+
+                case "Update Employee Manager":
+
+                    break;
+
+                case "Add New Role":
+
+                    break;
+
+                case "Remove Role":
+
+                    break;
+
+                default:
+                    break;
+            }
         })
-        //console.log(input)
-    }
+    //  }
 }
 
 main();
